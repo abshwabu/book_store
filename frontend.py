bddbfdef44fa96b3f1d008cc5d97b9dec9d64e42
ashwabu <abdulazizredi90@gmail.com>
2023-07-02 03:17:27 +0300

    edited

--- frontend.py
+++ frontend.py
@@ -2,16 +2,16 @@ from tkinter import *
 
 window = Tk()
 
-title = lable(window,text='title')
+title = Label(window,text='title')
 title.grid(row=0,column=0)
 
-author = lable(window,text='author')
-author.grid(row=0,column=0)
+author = Label(window,text='author')
+author.grid(row=0,column=2)
 
-year = lable(window,text='year')
-year.grid(row=0,column=0)
+year = Label(window,text='year')
+year.grid(row=1,column=0)
 
-isbn = lable(window,text='isbn')
-isbn.grid(row=0,column=0)
+isbn = Label(window,text='isbn')
+isbn.grid(row=1,column=2)
 
 window.mainloop()
